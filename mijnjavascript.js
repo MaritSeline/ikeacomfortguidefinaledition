bdaaca70abaacb3268cf3eb6d1cf07adda641975
Marit <m.s.vangrinsven@student.tudelft.nl>
2021-05-24 20:45:37 +0200

    image mattress

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -24,7 +24,7 @@ function nextQuestion(clicked_id) {
   var storage = sessionStorage.getItem(classnamen[0]);
   document.getElementById(classnamen[0]+"tag").innerHTML = storage;
   savedtag = storage;
-  document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage'>";
+  document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
   otherChapter("rightbutton");
 }
 
@@ -94,12 +94,12 @@ function tagmouseover(this_id){
   savedtag = document.getElementById(elementnaam + "tag").innerHTML;
   document.getElementById(elementnaam + "tag").innerHTML = this_id;
   if(elementnaam != "typesize"){
-  document.getElementById("imageresult").innerHTML = "<img src='images/mattress_"+ this_id + ".png' alt='' class='' id='defaultimage'>";
+  document.getElementById("imageresult").innerHTML = "<img src='images/mattress_"+ this_id + ".png' alt='' class='' id='defaultimage' width='100%'>";
   }
 }
 function tagmouseout(this_id){
   var tagelement = document.getElementById(this_id);
   var elementnaam = tagelement.className.split(" ")[0];
   document.getElementById(elementnaam + "tag").innerHTML = savedtag;
-  document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage'>";
+  document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
 }
