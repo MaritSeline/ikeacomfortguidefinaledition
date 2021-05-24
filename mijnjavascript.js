36755fec237c520eae3145c294c0da93cf8b8d7b
Marit <m.s.vangrinsven@student.tudelft.nl>
2021-05-24 22:41:02 +0200

    beautify

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -5,22 +5,20 @@ function nextQuestion(clicked_id) {
   var cName = document.getElementById(clicked_id);
   var skip = false;
   var chapterboolean = false;
-  if(cName.className.includes("skip"))
-  {
+  if (cName.className.includes("skip")) {
     classnaam = cName.className.split(" ", 1);
     skip = true;
-  }
-  else {
+  } else {
     classnaam = cName.className;
   }
   var classnamen = cName.className.split(" ");
 
   sessionStorage.setItem(classnamen[0], clicked_id);
   var storage = sessionStorage.getItem(classnamen[0]);
-  document.getElementById(classnamen[0]+"tag").innerHTML = storage;
+  document.getElementById(classnamen[0] + "tag").innerHTML = storage;
   savedtag = storage;
   document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
-  if (classnamen[1] == "chapter1"){
+  if (classnamen[1] == "chapter1") {
     document.getElementById("left").style.width = "70%";
     document.getElementById("right").style.display = "block";
     document.getElementById("progressbar").style.width = "70%";
@@ -46,15 +44,12 @@ function sliderFunction(clicked_id) {
       rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
       classnamen = cName.className.split(" ");
       sessionStorage.setItem(classnamen[0], range.value);
-      if(classnamen[0] == "weightslider")
-      {
-        document.getElementById(classnamen[0]+"tag").innerHTML = range.value + "KG";
+      if (classnamen[0] == "weightslider") {
+        document.getElementById(classnamen[0] + "tag").innerHTML = range.value + "KG";
+      } else {
+        document.getElementById(classnamen[0] + "tag").innerHTML = range.value;
       }
-      else
-      {
-           document.getElementById(classnamen[0]+"tag").innerHTML = range.value;
-         }
-  };
+    };
   document.addEventListener("DOMContentLoaded", setValue);
   range.addEventListener('input', setValue);
 
@@ -63,19 +58,15 @@ function sliderFunction(clicked_id) {
 
 function otherChapter(clickedbutton) {
   var sChapter = parseInt(selectedChapter.split("chapter")[1]);
-  if(sChapter != 1)
-  {
+  if (sChapter != 1) {
     document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
   }
-  if(clickedbutton == "leftbutton")
-  { if(sChapter == 1){  }
-    else if(sChapter >1 && sChapter<=10){
+  if (clickedbutton == "leftbutton") {
+    if (sChapter == 1) {} else if (sChapter > 1 && sChapter <= 10) {
       sChapter = sChapter - 1;
     }
-  }
-  else if(clickedbutton == "rightbutton")
-  { if(sChapter == 10){  }
-    else if(sChapter >=1 && sChapter<10){
+  } else if (clickedbutton == "rightbutton") {
+    if (sChapter == 10) {} else if (sChapter >= 1 && sChapter < 10) {
       sChapter = sChapter + 1;
     }
   }
@@ -102,25 +93,26 @@ function loadonclickimage(thisbutton) {
   elementbutton.src = "images/bluedot.jpg";
   var sChapter = cid.split("chapter")[1];
   selectedChapter = "chapter" + sChapter;
-  if(sChapter == "1"){
+  if (sChapter == "1") {
     document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
   }
 }
 
-function tagmouseover(this_id){
+function tagmouseover(this_id) {
   var tagelement = document.getElementById(this_id);
   var elementnaam = tagelement.className.split(" ")[0];
   savedtag = document.getElementById(elementnaam + "tag").innerHTML;
   document.getElementById(elementnaam + "tag").innerHTML = this_id;
-  if(elementnaam != "typesize"){
-  document.getElementById("imageresult").innerHTML = "<img src='images/mattress_"+ this_id + ".png' alt='' class='' id='defaultimage' width='100%'>";
+  if (elementnaam != "typesize") {
+    document.getElementById("imageresult").innerHTML = "<img src='images/mattress_" + this_id + ".png' alt='' class='' id='defaultimage' width='100%'>";
   }
 }
-function tagmouseout(this_id){
+
+function tagmouseout(this_id) {
   var tagelement = document.getElementById(this_id);
   var elementnaam = tagelement.className.split(" ")[0];
   document.getElementById(elementnaam + "tag").innerHTML = savedtag;
-  if(elementnaam != "typesize"){
+  if (elementnaam != "typesize") {
     document.getElementById("imageresult").innerHTML = "<img src='images/mattress.png' alt='' class='' id='defaultimage' width='100%'>";
 
   }
