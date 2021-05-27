55c065d7e0da97cec1718f0e4579a6191354cd9b
Igor Zweerus <izweerus1@gmail.com>
2021-05-27 13:16:10 +0200

    Recommendation page image working

--- recommendation.js
+++ recommendation.js
@@ -1,16 +1,52 @@
+let bedframe = [1, 1.1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let bedbank = [1, 1.1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let loftbed = [1, 1.1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let column = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let triangle = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let oval = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let broad = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let muscular = [1.2, 0.5, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let side = [1, 0.5, 1.1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let back = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let belly = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let mixed = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let shoulders = [1.6, 1, 1.5, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let lowerback = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5.1, 0, 0];
+let neck = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let breast = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let upperback = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let cool = [1, 0.5, 1, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let normal = [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let warm = [1.5, 0, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let clean = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5.1, 0];
+let combine = [0, 0, 0, 0, 0, 5.1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let recycle = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5.1];
+let natural = [1, 0.5, 0.5, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let nomemoryfoam = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let nosprings = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let skipimportant = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let skipexclude = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let skipprice = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let skiphealth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let names = ["HOVAG", "MALFORS", "MYRBACKA", "VATNESTROM", "HAFSLO", "HAMARVIK", "HOKKASEN", "HYLLESTAD", "JOMNA", "MALVIK", "MATRAND", "MAUSUND", "MORGEDAL", "MOSHULT", "VADSO"];
+
+var score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var old = [];
+
+
 window.onload = function() {
-  alert(sessionStorage.length);
   for (var i = 0; i < sessionStorage.length; i++){
-    document.getElementById("results").innerHTML += "<p>" + sessionStorage.getItem(sessionStorage.key(i)) + "</p>";
-}
-
-  // Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    var key = sessionStorage.key(i);
+    alert(key);
+    document.getElementById(key+"tag").innerHTML = sessionStorage.getItem(key);
+    var currentscore = eval(sessionStorage.getItem(key));
+    for (var j = 0; j < score.length; j++) {
+      score[j] = score[j] + currentscore[j];
     }
-}
+  }
+  var imagescore = score.indexOf(Math.max(...score));
+  var imagename = names[imagescore];
+  document.getElementById("bestbed").src = "images/" + imagename + "_section.jpg";
+
 }
