d003180196ee4d84af163bac204d87f0f2ff08ce
Igor Zweerus <izweerus1@gmail.com>
2021-05-30 21:27:21 +0200

    Recommendation werkt beter

--- recommendation.js
+++ recommendation.js
@@ -5,6 +5,10 @@ let _90 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let _120 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let _140 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let _180 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let ondergewicht = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let normaal = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let overgewicht = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let obesitas = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let side = [0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1];
 let back = [1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1];
 let belly = [1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0];
@@ -28,9 +32,12 @@ let skiphealth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 let names = ["HAFSLO", "HAMARVIK", "HOKKASEN", "HOVAG", "HYLLESTAD", "MALVIK", "MATRAND", "MAUSUND", "MORGEDAL", "MYRBACKA", "VADSO", "VATNESTROM"];
 
-var score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var old = [];
 
+var weight;
+var length;
+var wlcounter = -2;
 
 window.onload = function() {
 
@@ -40,17 +47,42 @@ window.onload = function() {
     document.getElementById(key+"tag").innerHTML = item;
 
     if(key == "weightslider")
-    {
-
+    { wlcounter++;
+      weight = item;
     }
     else if (key == "lengthslider") {
-
+      wlcounter++;
+      lenght = item;
     }
     else if (key == "priceslider") {
-
+      
     }
     else {
       var currentscore = eval(item.toLowerCase());
+      for (var j = 0; j < score.length; j++) {
+        score[j] = score[j] + currentscore[j];
+      }
+    }
+    if (wlcounter == 0)
+    {
+      var bmi = weight/(length*length);
+      var currentscore;
+      if (bmi < 18.5)
+      {
+        currentscore = eval("ondergewicht");
+      }
+      else if (bmi >= 18.5 && bmi <= 25)
+      {
+        currentscore = eval("normaal");
+      }
+      else if (bmi > 25 && bmi <= 30)
+      {
+        currentscore = eval("overgewicht");
+      }
+      else if (bmi > 30)
+      {
+        currentscore = eval("obesitas");
+      }
       for (var j = 0; j < score.length; j++) {
         score[j] = score[j] + currentscore[j];
       }
