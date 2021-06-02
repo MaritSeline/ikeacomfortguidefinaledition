b431565815b6ef56ac94be313abfb612ffbdb73f
Marit <m.s.vangrinsven@student.tudelft.nl>
2021-06-02 15:59:43 +0200

    testrec adjusted

--- frame.js
+++ frame.js
@@ -28,3 +28,10 @@ $(window).resize(updateScale);
 
 // Get the modal
 var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
