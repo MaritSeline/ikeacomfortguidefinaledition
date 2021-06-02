0ccd75a48ed4593359735d26907ebc191f035884
Igor Zweerus <izweerus1@gmail.com>
2021-06-02 20:54:24 +0200

    Specific text at checkout

--- checkout.js
+++ checkout.js
@@ -2,4 +2,13 @@
 window.onload = function() {
   document.getElementById("titlerec").textContent = localStorage.getItem("name");
   document.getElementById("selectedmattrass").src = "images/" + localStorage.getItem("name") + "_section.png";
+  document.getElementById("pricerec").textContent = localStorage.getItem("price");
+  document.getElementById("matchrec").textContent = localStorage.getItem("tags");
+
+  document.getElementById(localStorage.getItem("name")).style.display = "block";
+
+
+
+
+
 }
